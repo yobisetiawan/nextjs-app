61f9615b4c6e6d2ed262806139d106fad635ef18
Yobi Bina Setiawan <yobi@62teknologi.com>
2023-05-25 00:57:52 +0700

    update layout

--- app/administrator/layout.tsx
+++ app/administrator/layout.tsx
@@ -1,6 +1,6 @@
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="admin-layoout">
+    <div className="admin-layout">
       <div>Admin Layout</div>
       {children}
     </div>
--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,8 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Base App',
-  description: 'Base App - generated',
+  title: 'App',
+  description: 'App - generated',
 }
 
 export default function RootLayout({
